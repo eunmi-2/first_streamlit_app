c4b4d744dd19fde27b1dba525d8ac65a715c6055
eunmi-2 <148329889+eunmi-2@users.noreply.github.com>
2023-10-19 09:51:45 +0900

    Update streamlit_app.py
    Fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamilt.text('🥑🍞 Avocado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
